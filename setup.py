bd8db931265069853ebbb4e2477e923f4f534fb5
gopichandu-tech <chandugopi196@gmail.com>
2021-10-23 19:31:32 +0530

    version update

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ USER_NAME = "gopichandu-tech"
 
 setuptools.setup(
     name=f"{PROJECT_NAME}-{USER_NAME}",
-    version="0.0.1",
+    version="0.0.2",
     author="Example Author",
     author_email="author@example.com",
     description="A small example package",
